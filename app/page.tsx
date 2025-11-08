82d46d95259e6df58b015097f16be559e45e56c6
codes-swalih <codesswalih@gmail.com>
2025-11-08 12:50:01 +0530

    fix test naviagation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
   return (
-   <div className=" w-full h-screen flex items-center justify-center">
+    <div className=" w-full h-screen flex items-center justify-center">
       <h1>test</h1>
-   </div>
+    </div>
   );
 }
